1fecf8e95dce0fce70e18bb5dfece8b9ec178cf8
Travis Hohl <travis@travishohl.com>
2014-09-13 10:56:37 -0500

    Pass test: whether "(]" returns false.
    Start using #inject to create a histogram, then compare the number of
    open braces of a type to the number of close braces of that same type.

--- braces_matching.rb
+++ braces_matching.rb
@@ -1,7 +1,19 @@
 # Reopen String class and define a new instance method
 class String
 	def braces_matching?
-		#TODO
-		return true
+
+		# Create an array of braces
+		braces_array = self.split('')
+
+		# Create histogram of types of braces
+		braces_histogram = braces_array.inject(Hash.new(0)) { |hash, count| hash[count] += 1; hash }
+
+		# Compare count of open and close braces, if
+		# equal, then that type of brace is matching
+		if (braces_histogram["("] == braces_histogram[")"])
+			return true
+		else
+			return false
+		end
 	end
 end
--- spec/braces_matching_spec.rb
+++ spec/braces_matching_spec.rb
@@ -1,8 +1,13 @@
 require_relative "../braces_matching"
 
 describe String, "#braces_matching?" do
-	it "given '()' it returns true" do
+	it "returns true given '()'" do
 		result = "()".braces_matching?
 		expect(result).to eq(true)
+	end
+
+	it "returns false given '(]'" do
+		result = "(]".braces_matching?
+		expect(result).to eq(false)
 	end
 end
